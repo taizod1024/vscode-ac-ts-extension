1ca671024912b0b9f430270272e0e72ed105fd11
yamamoto.taizo <taizod1024@gmail.com>
2021-06-13 15:03:35 +0900

    build: pre-commitフック修正

--- bin/check_no_main_branch.sh
+++ bin/check_no_main_branch.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-# if the branch is master, then fail.
+# if the branch is main, then fail.
 
 branch="$(git symbolic-ref HEAD 2>/dev/null)" || \
        "$(git describe --contains --all HEAD)"
 last_comment="$(git log -n 1 --pretty=%s)"
 
-echo "pre-commit:"
 if [ "${branch##refs/heads/}" = "main" ]; then
   if [ "${last_comment}" != "Merge branch 'develop'" ]; then
+    echo "=> ng(main branch)"
     echo "*** do not commit on the main branch ***"
     exit 1
   else
